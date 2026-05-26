e08dcae901e42a167c7bbd08e10b9536e6486ff6
cahyo <ncahyooo69@gmail.com>
2026-05-27 03:12:15 +0700

    chore: cleanup unused files, packages, and imports

--- backend/test.py
+++
@@ -1,5 +0,0 @@
-import psycopg2
-conn = psycopg2.connect('postgresql://postgres:jimametmbkm6@db.mldkftqbtmlkcmjganmj.supabase.co:5432/postgres')
-cur = conn.cursor()
-cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name = 'users'")
-print([r[0] for r in cur.fetchall()])
